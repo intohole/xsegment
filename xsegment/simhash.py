4ffe776941128ab1008d1998c10cbf9c961b947c
intohole <intoblack86@gmail.com>
2015-06-06 17:35:54 +0800

    fix simhash code style

--- xsegment/simhash.py
+++ xsegment/simhash.py
@@ -3,9 +3,11 @@
 
 
 from collections import defaultdict
+from ctypes import c_uint64
 
 
 class SimHash(object):
+
     '''
     simhash 海明距离计算
     词——》 hash
@@ -14,8 +16,8 @@ class SimHash(object):
     取反取距离
     '''
 
-    __seg = None #分词藉口
-    __word_hash = defaultdict() #word——hash 保存
+    __seg = None  # 分词藉口
+    word_hash_dict = defaultdict()  # word——hash 保存
 
     def __init__(self, segfun=None):
         self.__seg = segfun
@@ -24,56 +26,45 @@ class SimHash(object):
         '''
         计算海明编码
         '''
-        if document:
-            if isinstance(document, (str, unicode)):
-                words = self.__seg(document) #
-                wm = defaultdict(int) #词hash->出现次数
-                for word in words: #循环分词
-                    if not self.__word_hash.has_key(word):
-                        self.__word_hash[word] = self.hash(word)
-                    wm[self.__word_hash[word]] += 1
-                hash_arry = [0 for i in range(64)] #所有ｈａｓｈ值相加减数组
-                for __hash , __weight in wm.items():#循环数组　权重
-                    self.__array_add(hash_arry , self.__toarry(__hash , __weight))
-                return self.__toint(hash_arry) #转换为数字
+        if document and isinstance(document, (str, unicode)):
+            words = self.__seg(document)
+            wm = defaultdict(int)  # 词hash->出现次数
+            for word in words:  # 循环分词
+                if word not in self.word_hash_dict:
+                    self.word_hash_dict[word] = self.hash(word)
+                wm[self.word_hash_dict[word]] += 1
+            hash_array = [0 for i in range(64)]  # 所有ｈａｓｈ值相加减数组
+            for hash_num, weight in wm.items():  # 循环数组　权重
+                hash_array = self.hash_array_add(
+                    hash_array, self.to_array(hash_num, weight))
+            return self.array_to_int(hash_array)  # 转换为数字
+        raise ValueError , "document is string !"
 
-    def __toint(self , hash_arry):
-        __h = 0 
-        for i in range(len(hash_arry)):
-            if hash_arry[i] > 0:
-                __h += (1 << i)
-        return __h
+    def array_to_int(self, hash_array):
+        if isinstance(hash_array, (tuple, list)):
+            return sum(1 << i for i in range(len(hash_array)) if hash_array[i] > 0)
+        raise TypeError, "hash_array type is list or tuple "
 
+    def hash_array_add(self, hash_array1, hash_array2):
+        if len(hash_array1) == len(hash_array2) == 64:
+            return [hash_array1[i] + hash_array2[i] for i in range(64)]
+        raise ValueError
 
-    def __array_add(self, arry1, arry2):
-        for i in range(len(arry1)):
-            arry1[i] += arry2[i]
-
-
-    def __toarry(self, h, weight):
-        __hash = [(h >> i) & 1 for i in range(64)]
-        for i in range(64):
-            if __hash[i]:
-                __hash[i] = weight
-            else:
-                __hash[i] = -1 * weight
-        return __hash
+    def to_array(self, hash_num, weight):
+        return [-weight if (hash_num >> i) == 0 else weight for i in range(64)]
 
     def hash(self, word):
         '''
         对字符串ｈａｓｈ作用
 
         '''
-        if word:
-            if isinstance(word, (str, unicode)):
-                return hash(word)
-        return None
+        return hash(word) & 0xFFFFFFFF 
 
     @staticmethod
-    def distance(sh1 , sh2):
-        if not (isinstance(sh1 , int ) and isinstance(sh2 , int)):
-            raise TypeError , '参数必须为整数 !'
-        h = (sh1 ^ sh2 ) & ( 1 << 64 - 1)
+    def distance(sh1, sh2):
+        if not (isinstance(sh1, (int , long )) and isinstance(sh2, (int,long))):
+            raise TypeError, '参数必须为整数 !'
+        h = (sh1 ^ sh2) & (1 << 64 - 1)
         d = 0
         while h:
             h = h & (h - 1)
@@ -82,10 +73,10 @@ class SimHash(object):
 
 
 if __name__ == '__main__':
-    f = SimHash(lambda x : x.split())
+    f = SimHash(lambda x: x.split())
     print f.figureprint('hello')
     print f.figureprint('i have a box !') ^ f.figureprint('i have a cat !')
     print f.figureprint('he have cat')
     print f.figureprint('i have a cat !')
-    print SimHash.distance(f.figureprint('he have cat !')  , f.figureprint('i have a cat !'))
+    print SimHash.distance(f.figureprint('he have cat !'), f.figureprint('i have a cat !'))
     print f.hash('a')
